fcc49a98b37d2fa1e2857ff3775403e81400969a
Duveshka <duveshka@mail.ru>
2020-02-20 20:07:50 +0300

    add empty NgRx files

--- ArcadiaParties-SPA/src/app/app.module.ts
+++ ArcadiaParties-SPA/src/app/app.module.ts
@@ -8,6 +8,7 @@ import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { environment } from '../environments/environment';
 import { EffectsModule } from '@ngrx/effects';
 import { AppEffects } from './app.effects';
+import { StoreRouterConnectingModule } from '@ngrx/router-store';
 
 @NgModule({
   declarations: [
