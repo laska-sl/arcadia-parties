97f65f192763a4378215d7402ed8b26161e3a260
Lasukov <vyacheslav.lasukov@arcadia.spb.ru>
2020-02-20 19:06:08 +0300

    Installed Swashbuckle and MediatR packages

--- ArcadiaParties.API/Startup.cs
+++ ArcadiaParties.API/Startup.cs
@@ -26,6 +26,7 @@ namespace ArcadiaParties.API
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -35,8 +36,6 @@ namespace ArcadiaParties.API
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.UseHttpsRedirection();
 
             app.UseRouting();
 
