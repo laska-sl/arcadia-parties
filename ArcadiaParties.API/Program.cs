de0e4ec189d4c6dff19a9d35cb9b313c99280a13
laska_sl <lasukovv902@gmail.com>
2020-02-22 18:12:29 +0300

    Added CQSR components

--- ArcadiaParties.API/Program.cs
+++ ArcadiaParties.API/Program.cs
@@ -1,19 +1,18 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
+using MediatR;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using ArcadiaParties.CQRS;
+using ArcadiaParties.CQRS.Queries;
 
 namespace ArcadiaParties.API
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
             var host = CreateHostBuilder(args).Build();
             //using (var scope = host.Services.CreateScope())
@@ -30,7 +29,26 @@ namespace ArcadiaParties.API
             //        logger.LogError(ex, "An error occured during migration");
             //    }
             //}
-            host.Run();
+
+            using (var scope = host.Services.CreateScope())
+            {
+                var services = scope.ServiceProvider;
+                try
+                {
+                    var mediator = services.GetRequiredService<IMediator>();
+
+                    // TODO: Pass argument to ctor
+                    var seedDataQuery = new SeedDataQuery();
+
+                    await mediator.Send(seedDataQuery);
+                }
+                catch (Exception ex)
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "An error occured during migration");
+                }
+            }
+            await host.RunAsync();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
---
+++ ArcadiaParties.CQRS/Responses/SeedDataResponse.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArcadiaParties.CQRS.Responses
+{
+    public class SeedDataResponse
+    {
+    }
+}
