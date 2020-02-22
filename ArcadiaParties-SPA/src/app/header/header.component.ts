5ac0975bce01b08e0b094acdddf2b15194d3d3a6
laska_sl <lasukovv902@gmail.com>
2020-02-22 23:55:25 +0300

    Code style changes

--- ArcadiaParties-SPA/src/app/header/header.component.ts
+++ ArcadiaParties-SPA/src/app/header/header.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Store, select } from '@ngrx/store';
-import { State } from '../reducers/reducers';
-
 import { Observable } from 'rxjs';
+
+import { State } from '../reducers/reducers';
 import { selectTitle } from '../selector/selector';
 
 @Component({
@@ -16,7 +16,6 @@ export class HeaderComponent implements OnInit {
   constructor(private store: Store<State>) { }
 
   ngOnInit() {
-
   }
 
 }
