812afebff7ae3e08d18ab45dc4f9e8dc7d9c0cc0
laska_sl <lasukovv902@gmail.com>
2020-02-22 20:29:13 +0300

    Revert "Added CQSR components"
    This reverts commit de0e4ec189d4c6dff19a9d35cb9b313c99280a13.

--- ArcadiaParties.API/Program.cs
+++ ArcadiaParties.API/Program.cs
@@ -1,18 +1,19 @@
 using System;
-using MediatR;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.DependencyInjection;
-using ArcadiaParties.CQRS;
-using ArcadiaParties.CQRS.Queries;
 
 namespace ArcadiaParties.API
 {
     public class Program
     {
-        public static async Task Main(string[] args)
+        public static void Main(string[] args)
         {
             var host = CreateHostBuilder(args).Build();
             //using (var scope = host.Services.CreateScope())
@@ -29,26 +30,7 @@ namespace ArcadiaParties.API
             //        logger.LogError(ex, "An error occured during migration");
             //    }
             //}
-
-            using (var scope = host.Services.CreateScope())
-            {
-                var services = scope.ServiceProvider;
-                try
-                {
-                    var mediator = services.GetRequiredService<IMediator>();
-
-                    // TODO: Pass argument to ctor
-                    var seedDataQuery = new SeedDataQuery();
-
-                    await mediator.Send(seedDataQuery);
-                }
-                catch (Exception ex)
-                {
-                    var logger = services.GetRequiredService<ILogger<Program>>();
-                    logger.LogError(ex, "An error occured during migration");
-                }
-            }
-            await host.RunAsync();
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
