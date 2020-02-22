7680e83c7d76e64bd42ee558302630b95165b108
laska_sl <lasukovv902@gmail.com>
2020-02-22 14:42:06 +0300

    Installed Material, added header component

--- ArcadiaParties-SPA/src/app/app.module.ts
+++ ArcadiaParties-SPA/src/app/app.module.ts
@@ -2,26 +2,33 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { MaterialModule } from './material/material.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppComponent } from './app.component';
 import { ROOT_REDUCERS } from './reducers/reducers';
 import { environment } from '../environments/environment';
+import { HeaderComponent } from './header/header.component';
+import { ContentComponent } from './content/content.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    StoreModule.forRoot(ROOT_REDUCERS, {
-      runtimeChecks: {
-        strictStateImmutability: true,
-        strictActionImmutability: true
-      }
-    }),
-    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      HeaderComponent,
+      ContentComponent
+   ],
+   imports: [
+      StoreModule.forRoot(ROOT_REDUCERS, {
+         runtimeChecks: {
+            strictStateImmutability: true,
+            strictActionImmutability: true
+         }
+      }),
+      BrowserModule,
+      BrowserAnimationsModule, StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
+      MaterialModule
+   ],
+   providers: [],
+   bootstrap: [AppComponent]
 })
 export class AppModule { }
