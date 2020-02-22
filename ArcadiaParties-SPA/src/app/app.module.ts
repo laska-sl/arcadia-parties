b912ac191f748dbb65a6476a67d0b6df5493ce12
laska_sl <lasukovv902@gmail.com>
2020-02-22 14:13:25 +0300

    Revert "Added header component"
    This reverts commit 05a279e896ef881df1ad832aea42cf397d7efed2.

--- ArcadiaParties-SPA/src/app/app.module.ts
+++ ArcadiaParties-SPA/src/app/app.module.ts
@@ -6,26 +6,20 @@ import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { AppComponent } from './app.component';
 import { ROOT_REDUCERS } from './reducers/reducers';
 import { environment } from '../environments/environment';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HeaderComponent } from './header/header.component';
-import { MaterialModule } from './material/material.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HeaderComponent
+    AppComponent
   ],
   imports: [
+    BrowserModule,
     StoreModule.forRoot(ROOT_REDUCERS, {
       runtimeChecks: {
         strictStateImmutability: true,
         strictActionImmutability: true
       }
     }),
-    BrowserModule,
-    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
-    BrowserAnimationsModule,
-    MaterialModule
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })
   ],
   providers: [],
   bootstrap: [AppComponent]
