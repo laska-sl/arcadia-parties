6ca086657dae932178049fec25d007bdc6514d11
laska_sl <lasukovv902@gmail.com>
2020-02-23 12:35:00 +0300

    Internal modules after external

--- ArcadiaParties-SPA/src/app/app.module.ts
+++ ArcadiaParties-SPA/src/app/app.module.ts
@@ -2,8 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { MaterialModule } from './material/material.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { MaterialModule } from './material/material.module';
 
 import { AppComponent } from './app.component';
 import { ROOT_REDUCERS } from './reducers/reducers';
