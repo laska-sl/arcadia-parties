cfd1289090e9ffece60a146831d99feaae698ec6
laska_sl <lasukovv902@gmail.com>
2020-02-23 18:00:13 +0300

    Empty line fix

--- ArcadiaParties-SPA/src/app/app.module.ts
+++ ArcadiaParties-SPA/src/app/app.module.ts
@@ -3,8 +3,8 @@ import { NgModule } from '@angular/core';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MaterialModule } from './material/material.module';
 
+import { MaterialModule } from './material/material.module';
 import { AppComponent } from './app.component';
 import { ROOT_REDUCERS } from './reducers/reducers';
 import { environment } from '../environments/environment';
