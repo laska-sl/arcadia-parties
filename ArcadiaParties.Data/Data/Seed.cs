6fa87719bd2725729df05d566431c2a4e9013d79
Duveshka <duveshka@mail.ru>
2020-02-23 20:59:26 +0300

    Seed Data

--- ArcadiaParties.Data/Data/Seed.cs
+++ ArcadiaParties.Data/Data/Seed.cs
@@ -1,5 +1,7 @@
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using ArcadiaParties.Data.Models;
 using Newtonsoft.Json;
 
@@ -8,20 +10,54 @@ namespace ArcadiaParties.Data.Data
 {
     public class Seed
     {
-        public static void SeedUsers(DataContext context)
+        private const bool V = true;
+
+        public async static Task<Boolean> SeedData(DataContext context)
         {
+            if (!context.Department.Any())
+            {
+                var depData = System.IO.File.ReadAllText("../ArcadiaParties.Data/Data/Depart.json");
+                var deps = JsonConvert.DeserializeObject<List<Department>>(depData);
+                foreach (var dep in deps)
+                {
+                    await context.Department.AddAsync(dep);
+                    await context.SaveChangesAsync();
+                }
+            }
+
+            if (!context.Roles.Any())
+            {
+                var roleData = System.IO.File.ReadAllText("../ArcadiaParties.Data/Data/RoleSeed.json");
+                var roles = JsonConvert.DeserializeObject<List<Role>>(roleData);
+                foreach (var role in roles)
+                {
+                    await context.Roles.AddAsync(role);
+                    await context.SaveChangesAsync();
+                }
+            }
+
             if (!context.Users.Any())
             {
                 var userData = System.IO.File.ReadAllText("../ArcadiaParties.Data/Data/UserSeed.json");
                 var users = JsonConvert.DeserializeObject<List<User>>(userData);
                 foreach (var user in users)
                 {
-                    context.Users.Add(user);
+                    await context.Users.AddAsync(user);
+                    await context.SaveChangesAsync();
                 }
-
-                context.SaveChanges(); 
             }
-        }
 
+            if (!context.UserRole.Any())
+            {
+                var userroleData = System.IO.File.ReadAllText("../ArcadiaParties.Data/Data/UserRole.json");
+                var usersroles = JsonConvert.DeserializeObject<List<UserRole>>(userroleData);
+                foreach (var ur in usersroles)
+                {
+                    await context.UserRole.AddAsync(ur);
+                    await context.SaveChangesAsync();
+                }
+            }
+            return true;
+        }
     }
 }
