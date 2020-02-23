38967e61e90e438f92b2e26adc9d52cdcd000474
Duveshka <duveshka@mail.ru>
2020-02-23 20:59:54 +0300

    Seed with MediatR

--- ArcadiaParties.CQRS/Handlers/SeedHandler.cs
+++ ArcadiaParties.CQRS/Handlers/SeedHandler.cs
@@ -1,14 +1,28 @@
-using ArcadiaParties.Data;
+using System.Threading;
+using System.Threading.Tasks;
 using ArcadiaParties.CQRS.Commands;
+using ArcadiaParties.Data;
 using ArcadiaParties.Data.Data;
 using MediatR;
-using System.Threading.Tasks;
-using System.Threading;
+using Microsoft.EntityFrameworkCore;
 
 namespace ArcadiaParties.CQRS.Handlers
 {
-    public class SeedHandler 
+    public class SeedHandler : IRequestHandler<SeedCommand>
     {
-     
+        private readonly DataContext _context;
+
+        public SeedHandler(DataContext context)
+        {
+            _context = context;
+        }
+
+
+        public async Task<Unit> Handle(SeedCommand request, CancellationToken cancellationToken)
+        {
+            await _context.Database.MigrateAsync();
+            await Seed.SeedData(_context);
+            return Unit.Value;
+        }
     }
 }
