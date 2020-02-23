6eafa88a7a98268318e972ba82803123707fd18c
laska_sl <lasukovv902@gmail.com>
2020-02-23 09:49:42 +0300

    App module formatting

--- ArcadiaParties-SPA/src/app/app.module.ts
+++ ArcadiaParties-SPA/src/app/app.module.ts
@@ -1,35 +1,34 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { StoreModule } from '@ngrx/store';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-import { MaterialModule } from './material/material.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { StoreModule } from "@ngrx/store";
+import { StoreDevtoolsModule } from "@ngrx/store-devtools";
+import { MaterialModule } from "./material/material.module";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
-import { AppComponent } from './app.component';
-import { ROOT_REDUCERS } from './reducers/reducers';
-import { environment } from '../environments/environment';
-import { HeaderComponent } from './header/header.component';
-import { ContentComponent } from './content/content.component';
+import { AppComponent } from "./app.component";
+import { ROOT_REDUCERS } from "./reducers/reducers";
+import { environment } from "../environments/environment";
+import { HeaderComponent } from "./header/header.component";
+import { ContentComponent } from "./content/content.component";
 
 @NgModule({
-   declarations: [
-      AppComponent,
-      HeaderComponent,
-      ContentComponent
-   ],
-   imports: [
-      StoreModule.forRoot(ROOT_REDUCERS, {
-         runtimeChecks: {
-            strictStateImmutability: true,
-            strictActionImmutability: true
-         }
-      }),
-      BrowserModule,
-      BrowserAnimationsModule,
-      StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
-      MaterialModule
-   ],
-   providers: [],
-   bootstrap: [AppComponent]
+  declarations: [AppComponent, HeaderComponent, ContentComponent],
+  imports: [
+    StoreModule.forRoot(ROOT_REDUCERS, {
+      runtimeChecks: {
+        strictStateImmutability: true,
+        strictActionImmutability: true
+      }
+    }),
+    BrowserModule,
+    BrowserAnimationsModule,
+    StoreDevtoolsModule.instrument({
+      maxAge: 25,
+      logOnly: environment.production
+    }),
+    MaterialModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
