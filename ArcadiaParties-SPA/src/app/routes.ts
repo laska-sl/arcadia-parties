5608c88bef259a4b3eb8b01cfc52b0089eff7167
Vyacheslav Lasukov <lasukovv902@gmail.com>
2020-02-25 13:01:12 +0300

    Removed unnecessary import, removed children

--- ArcadiaParties-SPA/src/app/routes.ts
+++ ArcadiaParties-SPA/src/app/routes.ts
@@ -1,13 +1,7 @@
 import { Routes } from '@angular/router';
-import { AppComponent } from './app.component';
 import { ContentComponent } from './content/content.component';
 
 export const appRoutes: Routes = [
-  {
-    path: 'home',
-    children: [
-      { path: '', component: ContentComponent }
-    ]
-  },
+  { path: 'home', component: ContentComponent },
   { path: '**', redirectTo: 'home', pathMatch: 'full' }
 ];
