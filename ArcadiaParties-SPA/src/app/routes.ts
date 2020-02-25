e943fab7c4cb0953e4c701bce8582182d90cc344
Vyacheslav Lasukov <lasukovv902@gmail.com>
2020-02-25 11:43:17 +0300

    Added children routes and router-outlet

--- ArcadiaParties-SPA/src/app/routes.ts
+++ ArcadiaParties-SPA/src/app/routes.ts
@@ -1,7 +1,13 @@
 import { Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { ContentComponent } from './content/content.component';
 
 export const appRoutes: Routes = [
-  { path: 'home', component: AppComponent },
+  {
+    path: 'home',
+    children: [
+      { path: '', component: ContentComponent }
+    ]
+  },
   { path: '**', redirectTo: 'home', pathMatch: 'full' }
 ];
