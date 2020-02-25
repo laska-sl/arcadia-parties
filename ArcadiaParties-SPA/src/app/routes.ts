40fc6bedfe6814f331e9467db703361bd53da7c2
Duveshka <duveshka@mail.ru>
2020-02-25 18:09:32 +0300

    add routes to user page

--- ArcadiaParties-SPA/src/app/routes.ts
+++ ArcadiaParties-SPA/src/app/routes.ts
@@ -1,7 +1,14 @@
 import { Routes } from '@angular/router';
 import { ContentComponent } from './content/content.component';
+import { UserInfoComponent } from './user/user-info/user-info.component';
 
 export const appRoutes: Routes = [
-  { path: 'home', component: ContentComponent },
+  {
+    path: 'home', component: ContentComponent,
+    children: [
+      { path: 'user', component: UserInfoComponent },
+    ]
+  },
+  
   { path: '**', redirectTo: 'home', pathMatch: 'full' }
 ];
