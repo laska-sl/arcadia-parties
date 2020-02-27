73d7e9e5dc805052b48bdd03d49c51c4bc1f3b4f
Duveshka <duveshka@mail.ru>
2020-02-27 15:24:06 +0300

    change routes

--- ArcadiaParties-SPA/src/app/routes.ts
+++ ArcadiaParties-SPA/src/app/routes.ts
@@ -1,10 +1,8 @@
 import { Routes } from '@angular/router';
-import { ContentComponent } from './content/content.component';
-import { UserInfoComponent } from './user/user-info/user-info.component';
 
+import { ContentComponent } from './content/content.component';
 
 export const appRoutes: Routes = [
   { path: 'home', component: ContentComponent },
-  { path: 'user', component: UserInfoComponent },
   { path: '**', redirectTo: 'home', pathMatch: 'full' }
 ];
---
+++ ArcadiaParties-SPA/src/app/user/routes.ts
@@ -0,0 +1,7 @@
+import { Routes } from '@angular/router';
+
+import { UserInfoComponent } from './user-info/user-info.component';
+
+export const userRoutes: Routes = [
+  { path: 'user', component: UserInfoComponent }
+];
