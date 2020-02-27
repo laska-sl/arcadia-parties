c2b38140b8500f7414fbd5ba8c5122fd27cbcdeb
Duveshka <duveshka@mail.ru>
2020-02-27 11:21:12 +0300

    rename action

--- ArcadiaParties-SPA/src/app/user/actions/actions.ts
+++ ArcadiaParties-SPA/src/app/user/actions/actions.ts
@@ -1,4 +1,4 @@
 import { createAction, props } from '@ngrx/store';
 import { User } from 'src/app/models/User';
 
-export const loadUser = createAction('[Main] Change User', props<{ user: User }>());
+export const loadUser = createAction('[Main] Get User', props<{ user: User }>());
