83e09f44eaa47f804215f72aebfc1284b3a20149
Duveshka <duveshka@mail.ru>
2020-02-27 12:49:29 +0300

    change user identity component

--- ArcadiaParties-SPA/src/app/user/user-identity/user-identity.component.ts
+++ ArcadiaParties-SPA/src/app/user/user-identity/user-identity.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Store, select } from '@ngrx/store';
 import { Observable } from 'rxjs';
+
 import { loadUser } from '../actions/actions';
 import { State } from '../../reducers/reducers';
 import { selectUserIdentity } from '../selector/selector';
@@ -10,26 +11,10 @@ import { selectUserIdentity } from '../selector/selector';
   templateUrl: './user-identity.component.html',
   styleUrls: ['./user-identity.component.scss']
 })
-export class UserIdentityComponent implements OnInit {
+export class UserIdentityComponent {
   useridentity$: Observable<string> = this.store.pipe(select(selectUserIdentity));
 
   constructor(private store: Store<State>) {
-    store.dispatch(
-      loadUser({
-        user: {
-          identity: 'ekaterina.kuznetsova',
-          firstName: 'ekaterina',
-          lastName: 'kuznetsova',
-          department: 'horoshiy',
-          dates: [
-            { name: 'BirthDay', date: new Date('11.02.1944') },
-            { name: 'HireDay', date: new Date('03.12.1955') }
-          ],
-          roles: ['User', 'Admin']
-        }
-      })
-    );
+    store.dispatch(loadUser());
   }
-
-  ngOnInit() {}
 }
