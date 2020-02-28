a0a31ad5d456a633909dcd625d084f8a7d02921a
Vyacheslav Lasukov <lasukovv902@gmail.com>
2020-02-28 13:41:12 +0300

    Implemented get users via MediatR

---
+++ ArcadiaParties.CQRS/Handlers/GetAllUsersHandler.cs
@@ -0,0 +1,26 @@
+﻿using ArcadiaParties.CQRS.Queries;
+using ArcadiaParties.Data.Abstractions.DTOs;
+using ArcadiaParties.Data.Abstractions.Repositories;
+using MediatR;
+using System.Collections.Generic;
+using System.Security.Claims;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ArcadiaParties.CQRS.Handlers
+{
+    class GetAllUsersHandler : IRequestHandler<GetAllUsersQuery, IEnumerable<UserDTO>>
+    {
+        private readonly IUserRepository _repo;
+
+        public GetAllUsersHandler(IUserRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public async Task<IEnumerable<UserDTO>> Handle(GetAllUsersQuery request, CancellationToken cancellationToken)
+        {
+            return await _repo.GetUsers();
+        }
+    }
+}
---
+++ ArcadiaParties.CQRS/Queries/GetAllUsersQuery.cs
@@ -0,0 +1,12 @@
+﻿using ArcadiaParties.Data.Abstractions.DTOs;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArcadiaParties.CQRS.Queries
+{
+    public class GetAllUsersQuery : IRequest<IEnumerable<UserDTO>>
+    {
+    }
+}
