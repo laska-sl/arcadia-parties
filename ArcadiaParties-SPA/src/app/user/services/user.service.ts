5199adf94523222c9da53ecb323494d0c92c8289
Duveshka <duveshka@mail.ru>
2020-02-28 13:52:31 +0300

    add user service

---
+++ ArcadiaParties-SPA/src/app/user/services/user.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { User } from '../models/User';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+  mockUser = {
+    identity: 'ekaterina.kuznetsova',
+    firstName: 'ekaterina',
+    lastName: 'kuznetsova',
+    department: 'horosho',
+    dates: [
+      { name: 'BirthDay', date: new Date('11.02.1944') },
+      { name: 'HireDay', date: new Date('03.12.1955') }
+    ],
+    roles: ['User', 'Admin']
+  } as User;
+
+  public getUser(): User {
+    return this.mockUser;
+  }
+}
