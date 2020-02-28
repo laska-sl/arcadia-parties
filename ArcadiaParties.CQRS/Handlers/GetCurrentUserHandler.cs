a34dbbb66b73d7dfec1e3187ce2ac02ac459c57d
Vyacheslav Lasukov <lasukovv902@gmail.com>
2020-02-28 15:22:53 +0300

    Implemented getting current user via MediatR

---
+++ ArcadiaParties.CQRS/Handlers/GetCurrentUserHandler.cs
@@ -0,0 +1,24 @@
+﻿using ArcadiaParties.CQRS.Queries;
+using ArcadiaParties.Data.Abstractions.DTOs;
+using ArcadiaParties.Data.Abstractions.Repositories;
+using MediatR;
+using System.Security.Claims;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ArcadiaParties.CQRS.Handlers
+{
+    class GetCurrentUserHandler : IRequestHandler<GetCurrentUserQuery, UserDTO>
+    {
+        private readonly IUserRepository _repo;
+        public GetCurrentUserHandler(IUserRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public async Task<UserDTO> Handle(GetCurrentUserQuery request, CancellationToken cancellationToken)
+        {
+            return await _repo.GetUser(request.Identity);
+        }
+    }
+}
---
+++ ArcadiaParties.CQRS/Queries/GetCurrentUserQuery.cs
@@ -0,0 +1,15 @@
+﻿using ArcadiaParties.Data.Abstractions.DTOs;
+using MediatR;
+
+namespace ArcadiaParties.CQRS.Queries
+{
+    public class GetCurrentUserQuery : IRequest<UserDTO>
+    {
+        public GetCurrentUserQuery(string identity)
+        {
+            Identity = identity;
+        }
+
+        public string Identity { get; }
+    }
+}
