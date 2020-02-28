cf706b0fa907d787155d072eb2ee5a5fbd858c5c
Vyacheslav Lasukov <lasukovv902@gmail.com>
2020-02-28 15:26:00 +0300

    Completed user controller

--- ArcadiaParties.API/Controllers/UsersController.cs
+++ ArcadiaParties.API/Controllers/UsersController.cs
@@ -1,10 +1,7 @@
-﻿using ArcadiaParties.Data.Abstractions.Repositories;
-using AutoMapper;
+﻿using ArcadiaParties.CQRS.Queries;
+using MediatR;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace ArcadiaParties.API.Controllers
@@ -13,24 +10,32 @@ namespace ArcadiaParties.API.Controllers
     [ApiController]
     public class UsersController : ControllerBase
     {        
-        private readonly IUserRepository _repo;
-        public UsersController(IUserRepository repo)
+        private readonly IMediator _mediator;
+        public UsersController(IMediator mediator)
         {
-            _repo = repo;
+            _mediator = mediator;
         }
 
+        [Authorize(Roles = "Administrator")]
         [HttpGet]
+        [Route("GetUsers")]
         public async Task<IActionResult> GetUsers()
         {
-            var users = await _repo.GetUsers();
+            var query = new GetAllUsersQuery();
+
+            var users = _mediator.Send(query);
 
             return Ok(users);
         }
 
-        [HttpGet("{identity}")]
-        public async Task<IActionResult> GetUser(string identity)
+        [HttpGet]
+        [Route("GetCurrentUser")]
+        public async Task<IActionResult> GetCurrentUser()
         {
-            var user = await _repo.GetUser(identity);
+            var userIdentity = User.Identity.Name;
+            var query = new GetCurrentUserQuery(userIdentity);
+
+            var user = _mediator.Send(query);
 
             return Ok(user);
         }
