a95f78b7f0b6e9cbe9c5459c9d1c6cfd00a70276
laska_sl <lasukovv902@gmail.com>
2020-03-01 14:54:48 +0300

    Added temp string to check if select working

--- ArcadiaParties-SPA/src/app/content/content.component.ts
+++ ArcadiaParties-SPA/src/app/content/content.component.ts
@@ -1,16 +1,31 @@
-import { Component } from '@angular/core';
-import { Store } from '@ngrx/store';
+import { Component, OnInit } from '@angular/core';
+import { Store, select } from '@ngrx/store';
+import { ActivatedRoute } from '@angular/router';
+import { Observable } from 'rxjs';
 
 import { changeTitleAction } from '../actions/actions';
 import { State } from '../reducers/reducers';
+import { selectUser } from '../user/selector/selector';
+import { User } from '../user/models/User';
 
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
   styleUrls: ['./content.component.scss']
 })
-export class ContentComponent {
-  constructor(private store: Store<State>) {
+export class ContentComponent implements OnInit {
+  currentUser$: Observable<User> = this.store.pipe(select(selectUser));
+  tempString: string;
+
+  constructor(private store: Store<State>, private route: ActivatedRoute) {
     store.dispatch(changeTitleAction({ title: 'Arcadia Parties' }));
+  }
+
+  ngOnInit() {
+    this.currentUser$.subscribe(user => this.tempString = user.department);
+
+    this.route.params.subscribe(params => {
+      this.tempString = params['department'];
+    });
   }
 }
