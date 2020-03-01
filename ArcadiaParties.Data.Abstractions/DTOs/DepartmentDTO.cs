fbde356427844d08e3713d037c295ce5c5772e29
Duveshka <duveshka@mail.ru>
2020-03-01 17:15:08 +0300

    add repository and DTO for department

---
+++ ArcadiaParties.Data.Abstractions/DTOs/DepartmentDTO.cs
@@ -0,0 +1,7 @@
+﻿namespace ArcadiaParties.Data.Abstractions.DTOs
+{
+    public class DepartmentDTO
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ ArcadiaParties.Data.Abstractions/Repositories/IDepartmentRepository.cs
@@ -0,0 +1,11 @@
+﻿using ArcadiaParties.Data.Abstractions.DTOs;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ArcadiaParties.Data.Abstractions.Repositories
+{
+    public interface IDepartmentRepository
+    {
+        Task<IEnumerable<DepartmentDTO>> GetDepartments();
+    }
+}
