d27730de41bc5008684ce0d3166eac1f58ff8a62
Duveshka <duveshka@mail.ru>
2020-03-02 16:53:53 +0300

    Create Calendar Controller

---
+++ ArcadiaParties.API/Controllers/CalendarController.cs
@@ -0,0 +1,39 @@
+﻿using ArcadiaParties.CQRS.Queries;
+using ArcadiaParties.Data.Abstractions.DTOs;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Swashbuckle.AspNetCore.Annotations;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ArcadiaParties.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class CalendarController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public CalendarController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [ProducesResponseType(typeof(IEnumerable<UserForCalendarDTO>), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [SwaggerOperation(
+             Summary = "Returns users of department"
+        )]
+        [Authorize]
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetUsersOfDepartment(int id, CancellationToken cancellationToken)
+        {
+            var query = new GetUsersOfDepartmentQuery(id);
+            var users = await _mediator.Send(query, cancellationToken);
+
+            return Ok(users);
+        }
+    }
+}
