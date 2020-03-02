cacba234d4335301a5d43d9f6d1f86a13a37f635
Duveshka <duveshka@mail.ru>
2020-03-02 16:53:12 +0300

    Add UserOfDepartment query and handler

---
+++ ArcadiaParties.CQRS/Handlers/GetUsersOfDepartmentHandler.cs
@@ -0,0 +1,30 @@
+﻿using ArcadiaParties.CQRS.Queries;
+using ArcadiaParties.Data.Abstractions.DTOs;
+using ArcadiaParties.Data.Abstractions.Repositories;
+using AutoMapper;
+using MediatR;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ArcadiaParties.CQRS.Handlers
+{
+    internal class GetUsersOfDepartmentHandler : IRequestHandler<GetUsersOfDepartmentQuery, IEnumerable<UserForCalendarDTO>>
+    {
+        private readonly IUserRepository _repo;
+        private readonly IMapper _mapper;
+
+        public GetUsersOfDepartmentHandler(IUserRepository repo, IMapper mapper)
+        {
+            _repo = repo;
+            _mapper = mapper;
+        }
+        public async Task<IEnumerable<UserForCalendarDTO>> Handle(GetUsersOfDepartmentQuery request, CancellationToken cancellationToken)
+        {
+            var users = await _repo.GetUsersOfDepartment(request.DepartmentId);
+            var usersToReturn = _mapper.Map<List<UserForCalendarDTO>>(users);
+            return usersToReturn;
+
+        }
+    }
+}
---
+++ ArcadiaParties.CQRS/Queries/GetUsersOfDepartmentQuery.cs
@@ -0,0 +1,16 @@
+﻿using ArcadiaParties.Data.Abstractions.DTOs;
+using MediatR;
+using System.Collections.Generic;
+
+namespace ArcadiaParties.CQRS.Queries
+{
+    public class GetUsersOfDepartmentQuery : IRequest<IEnumerable<UserForCalendarDTO>>
+    {
+        public int DepartmentId { get; }
+        
+        public GetUsersOfDepartmentQuery(int departmentId)
+        {
+            DepartmentId = departmentId;
+        }
+    }
+}
