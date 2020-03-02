ca1ebf4b7a1d6a208cf31810b9ce625273234378
Duveshka <duveshka@mail.ru>
2020-03-02 16:54:02 +0300

    Add Map

--- ArcadiaParties.Data/Repositories/AutoMapperProfile.cs
+++ ArcadiaParties.Data/Repositories/AutoMapperProfile.cs
@@ -1,6 +1,7 @@
 ﻿using ArcadiaParties.Data.Abstractions.DTOs;
 using ArcadiaParties.Data.Models;
 using AutoMapper;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace ArcadiaParties.Data.Helpers
@@ -14,6 +15,26 @@ namespace ArcadiaParties.Data.Helpers
                 .ForMember(uDTO => uDTO.Department, userDtoOpt => userDtoOpt.MapFrom(user => user.Department.Name));
 
             CreateMap<Department, DepartmentDTO>();
+
+            CreateMap<UserDTO, UserForCalendarDTO>()
+                .ConstructUsing(user => new UserForCalendarDTO
+                {
+                    FirstName = user.FirstName,
+                    LastName = user.LastName,
+                    Dates = new List<CelebratingDateDTO>
+                    {
+                        new CelebratingDateDTO
+                        {
+                            DateName = "Birth Date",
+                            Date = user.BirthDate
+                        },
+                        new CelebratingDateDTO
+                        {
+                            DateName = "Hire Date",
+                            Date = user.HireDate
+                        }
+                    }
+                });
         }
     }
 }
