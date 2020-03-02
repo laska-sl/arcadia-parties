9614bebfd4c668be4cccf5a5fc0ca88597641ed7
Vyacheslav Lasukov <lasukovv902@gmail.com>
2020-03-02 14:07:52 +0300

    Added routes

--- ArcadiaParties-SPA/src/app/routes.ts
+++ ArcadiaParties-SPA/src/app/routes.ts
@@ -1,9 +1,27 @@
 import { Routes } from '@angular/router';
 
 import { ContentComponent } from './content/content.component';
+import { DepartmentIdentityComponent } from './department/department-identity/department-identity.component';
 
 export const appRoutes: Routes = [
-  { path: 'home', component: ContentComponent },
-  { path: 'home/:departmentId', component: ContentComponent },
+  {
+    path: 'home',
+    children: [
+      {
+        path: '',
+        component: ContentComponent
+      },
+      {
+        path: '',
+        outlet: 'departmentSelector',
+        component: DepartmentIdentityComponent
+      },
+      {
+        path: ':departmentId',
+        component: ContentComponent
+      }
+    ]
+  },
+  // { path: 'user', component: DepartmentIdentityComponent, outlet: 'departmentSelector' },
   { path: '**', redirectTo: 'home', pathMatch: 'full' }
 ];
