13ee0e3d029f2704a05e30514b0ff4c593f38748
laska_sl <lasukovv902@gmail.com>
2020-03-17 16:21:37 +0300

    Removed test buttons

--- ArcadiaParties-SPA/src/app/app.component.ts
+++ ArcadiaParties-SPA/src/app/app.component.ts
@@ -1,7 +1,4 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-
-import { AuthService } from './services/auth.service';
 
 @Component({
   selector: 'app-root',
@@ -9,21 +6,5 @@ import { AuthService } from './services/auth.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  str: string;
-  constructor(private http: HttpClient, private authService: AuthService) {}
-
-  login() {
-    this.authService.login();
-  }
-
-  apiMehtod() {
-    this.http.get('http://localhost:5000/api/Users/GetCurrentUser').subscribe(
-      data => console.log(data),
-      err => console.log(err)
-    );
-  }
-
-  logout() {
-    this.authService.logout();
-  }
+  constructor() {}
 }
