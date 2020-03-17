08ae51297876b6a433e7ca2c0d14d56204fcb90e
laska_sl <lasukovv902@gmail.com>
2020-03-17 19:06:56 +0300

    Changed using dep service in effect

--- ArcadiaParties-SPA/src/app/department/effects/effect.ts
+++ ArcadiaParties-SPA/src/app/department/effects/effect.ts
@@ -1,18 +1,22 @@
 import { createEffect, ofType, Actions } from '@ngrx/effects';
 import { Injectable } from '@angular/core';
-import { map } from 'rxjs/operators';
+import { map, mergeMap } from 'rxjs/operators';
 
 import { DepartmentService } from '../services/department.service';
 import { loadDepartmentsSuccessAction, loadDepartmentsAction } from '../actions/actions';
 
 @Injectable()
 export class DepartmentEffect {
-    loadDepartments = createEffect(() =>
-        this.actions.pipe(
-            ofType(loadDepartmentsAction),
-            map(() => loadDepartmentsSuccessAction({ departments: this.departmentService.getDepartments() }))
-        )
-    );
+  loadDepartments = createEffect(() =>
+    this.actions.pipe(
+      ofType(loadDepartmentsAction),
+      mergeMap(() =>
+        this.departmentService
+          .getDepartments()
+          .pipe(map(departmentsFromService => loadDepartmentsSuccessAction({ departments: departmentsFromService })))
+      )
+    )
+  );
 
-    constructor(private actions: Actions, private departmentService: DepartmentService) { }
+  constructor(private actions: Actions, private departmentService: DepartmentService) {}
 }
