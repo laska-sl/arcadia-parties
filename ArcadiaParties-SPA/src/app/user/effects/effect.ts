28a0a79bef058eff4f4307778595896f55b59967
Duveshka <duveshka@mail.ru>
2020-03-18 11:31:10 +0300

    change pipes

--- ArcadiaParties-SPA/src/app/user/effects/effect.ts
+++ ArcadiaParties-SPA/src/app/user/effects/effect.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { map, mergeMap } from 'rxjs/operators';
+import { map, switchMap } from 'rxjs/operators';
 
 import { UserService } from '../services/user.service';
 import { loadUserAction, loadUserSuccessAction } from '../actions/actions';
@@ -10,10 +10,8 @@ export class UserEffect {
   loadUser = createEffect(() =>
     this.actions.pipe(
       ofType(loadUserAction),
-      mergeMap(() => this.userService.getUser()
-      .pipe(
-        map(userFromService => loadUserSuccessAction({user : userFromService}))
-      ))
+      switchMap(() => this.userService.getUser()),
+      map(userFromService => loadUserSuccessAction({ user: userFromService }))
     )
   );
 
