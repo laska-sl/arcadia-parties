b5e964196f99cb159329149d16b324d2b3ad5e26
laska_sl <lasukovv902@gmail.com>
2020-03-19 15:51:01 +0300

    Changed getting API URL

--- ArcadiaParties-SPA/src/app/department/services/department.service.ts
+++ ArcadiaParties-SPA/src/app/department/services/department.service.ts
@@ -2,18 +2,17 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-import { environment } from 'src/environments/environment';
 import { Department } from '../models/department';
+import { AppSettingsService } from 'src/app/app-settings/app-settings.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class DepartmentService {
-  baseUrl = environment.apiUrl;
-
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private appSettingsService: AppSettingsService) { }
 
   getDepartments(): Observable<Department[]> {
-    return this.http.get<Department[]>(this.baseUrl + 'departments');
+    const apiUrl = this.appSettingsService.appSettings.apiUrl;
+    return this.http.get<Department[]>(apiUrl + 'departments');
   }
 }
