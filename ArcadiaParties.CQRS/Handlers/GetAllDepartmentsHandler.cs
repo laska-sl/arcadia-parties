ce294676852d8ed39cf6b34173b2be6857f0b4a7
Duveshka <duveshka@mail.ru>
2020-03-26 16:34:13 +0300

    change departments handler

--- ArcadiaParties.CQRS/Handlers/GetAllDepartmentsHandler.cs
+++ ArcadiaParties.CQRS/Handlers/GetAllDepartmentsHandler.cs
@@ -1,25 +1,48 @@
 using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
 using ArcadiaParties.CQRS.Queries;
 using ArcadiaParties.Data.Abstractions.DTOs;
 using ArcadiaParties.Data.Abstractions.Repositories;
+using AutoMapper;
 using MediatR;
 
 namespace ArcadiaParties.CQRS.Handlers
 {
     internal class GetAllDepartmentsHandler : IRequestHandler<GetAllDepartmentsQuery, IEnumerable<DepartmentDTO>>
     {
-        private readonly IDepartmentRepository _repo;
+        private const string departmentsUrl = "https://assistant.arcadia.spb.ru/api/departments";
+        private readonly IAssistantTokenRepository _tokenRepository;
+        private readonly IHttpClientFactory _clientFactory;
+        private readonly IMapper _mapper;
 
-        public GetAllDepartmentsHandler(IDepartmentRepository repo)
+        public GetAllDepartmentsHandler(IAssistantTokenRepository tokenRepository, IHttpClientFactory clientFactory, IMapper mapper)
         {
-            _repo = repo;
+            _tokenRepository = tokenRepository;
+            _clientFactory = clientFactory;
+            _mapper = mapper;
         }
 
         public async Task<IEnumerable<DepartmentDTO>> Handle(GetAllDepartmentsQuery request, CancellationToken cancellationToken)
         {
-            return await _repo.GetDepartments();
+            var httpRequest = new HttpRequestMessage(HttpMethod.Get, departmentsUrl);
+
+            var token = await _tokenRepository.GetToken();
+            httpRequest.Headers.Authorization = new AuthenticationHeaderValue("Bearer", token);
+            var client = _clientFactory.CreateClient();
+            var response = await client.SendAsync(httpRequest, cancellationToken);
+            var responseBody = await response.Content.ReadAsStreamAsync();
+
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true,
+            };
+            var assistantDepartments = await JsonSerializer.DeserializeAsync<IEnumerable<AssistantDepartmentDTO>>(responseBody, options);
+            var departmentToReturn = _mapper.Map<IEnumerable<DepartmentDTO>>(assistantDepartments);
+            return departmentToReturn;
         }
     }
 }
