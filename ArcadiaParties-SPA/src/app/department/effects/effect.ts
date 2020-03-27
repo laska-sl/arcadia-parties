8f9ca67441aa7beabdeb47325aa0cc794521c953
Duveshka <duveshka@mail.ru>
2020-03-27 16:04:21 +0300

    fix changing departmentId

--- ArcadiaParties-SPA/src/app/department/effects/effect.ts
+++ ArcadiaParties-SPA/src/app/department/effects/effect.ts
@@ -3,18 +3,25 @@ import { Injectable } from '@angular/core';
 import { map, switchMap } from 'rxjs/operators';
 
 import { DepartmentService } from '../services/department.service';
-import { loadDepartmentsSuccessAction, loadDepartmentsAction } from '../actions/actions';
+import { loadDepartmentsSuccessAction, loadDepartmentsAction, changeDepartmentIdAction } from '../actions/actions';
+import { loadUserSuccessAction } from 'src/app/user/actions/actions';
 
 @Injectable()
 export class DepartmentEffect {
+  changeDepartmentId = createEffect(() =>
+    this.actions.pipe(
+      ofType(loadUserSuccessAction),
+      map(props => changeDepartmentIdAction({ departmentId: props.user.department.id }))
+    )
+  );
+
   loadDepartments = createEffect(() =>
     this.actions.pipe(
       ofType(loadDepartmentsAction),
-      switchMap(() =>
-        this.departmentService
-          .getDepartments()),
-      map(departmentsFromService => loadDepartmentsSuccessAction({ departments: departmentsFromService })))
+      switchMap(() => this.departmentService.getDepartments()),
+      map(departmentsFromService => loadDepartmentsSuccessAction({ departments: departmentsFromService }))
+    )
   );
 
-  constructor(private actions: Actions, private departmentService: DepartmentService) { }
+  constructor(private actions: Actions, private departmentService: DepartmentService) {}
 }
