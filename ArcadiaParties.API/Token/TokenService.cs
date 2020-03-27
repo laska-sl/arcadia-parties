64106f9b446542496fff9c81ef008b94d2d1f35d
laska_sl <lasukovv902@gmail.com>
2020-03-27 10:06:25 +0300

    Implemented interfaces

---
+++ ArcadiaParties.API/Token/TokenService.cs
@@ -0,0 +1,40 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.IdentityModel.Clients.ActiveDirectory;
+
+namespace ArcadiaParties.API.Token
+{
+    public class TokenService : ITokenService
+    {
+        public TokenService(string mainAppClientId, AzureAdConfiguration targetAppConfiguration, IHttpContextAccessor httpContextAccessor)
+        {
+            MainAppClientId = mainAppClientId;
+            TargetAppConfiguration = targetAppConfiguration;
+            HttpContextAccessor = httpContextAccessor;
+        }
+
+        public string MainAppClientId { get; }
+
+        public AzureAdConfiguration TargetAppConfiguration { get; }
+
+        public IHttpContextAccessor HttpContextAccessor { get; }
+
+        public async Task<string> GetTokenAsync()
+        {
+            var accessToken = await HttpContextAccessor.HttpContext.GetTokenAsync("access_token");
+
+            string assertionType = "urn:ietf:params:oauth:grant-type:jwt-bearer";
+
+            var userAssertion = new UserAssertion(accessToken, assertionType);
+
+            var clientCredential = new ClientCredential(MainAppClientId, TargetAppConfiguration.ClientSecret);
+
+            var authContext = new AuthenticationContext(TargetAppConfiguration.AuthorityUrl, new TokenCache());
+
+            var result = await authContext.AcquireTokenAsync(TargetAppConfiguration.ClientId, clientCredential, userAssertion);
+
+            return result.AccessToken;
+        }
+    }
+}
