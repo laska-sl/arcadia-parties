fc13c2122f5e66ecf0eb9305102d767a765da87f
laska_sl <lasukovv902@gmail.com>
2020-03-27 11:46:02 +0300

    Moved oauth model to separate project

---
+++ ArcadiaParties.Configuration/OAuthSettings.cs
@@ -0,0 +1,13 @@
+﻿namespace ArcadiaParties.Configuration
+{
+    public class OAuthSettings
+    {
+        public string ClientId { get; set; }
+
+        public string AuthorizationUrl { get; set; }
+
+        public string TokenUrl { get; set; }
+
+        public string OpenIdConfigurationUrl { get; set; }
+    }
+}
